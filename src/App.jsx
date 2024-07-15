6beef988cd475419edc0316b3c892ac6f3cb0498
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-07-15 19:03:55 +0200

    feat: replace App.css with index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import './App.css';
+import './index.css';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import NavBar from './components/NavBar';
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="bg-black">
+      <NavBar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
 };
 
 export default App;
