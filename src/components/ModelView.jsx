f4ec6e4bfc0a8ba95aac0aae1d8e043978552771
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-07-16 11:35:26 +0200

    Enhance ModelView component with OrbitControls and dynamic group rendering
    - Added OrbitControls to the ModelView component for intuitive model interaction.
      - Disabled zoom and pan for focused rotation.
      - Set rotateSpeed to 0.4 for smooth rotation control.
      - Targeted the control at the scene origin (0, 0, 0).
      - Used onEnd callback to update the rotation state based on the azimuthal angle.
    - Introduced a group element to conditionally render small or large models based on the index prop.
      - Applied refs to the group for direct manipulation and tracking.
      - Set the group position to the origin.
    - Wrapped the IPhone component in a Suspense fallback to handle loading states.
      - Applied conditional scaling to the IPhone component based on the index prop.
      - Passed item and size props to IPhone for dynamic rendering.

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,5 +1,12 @@
 import { Suspense } from 'react';
-import { PerspectiveCamera, View } from '@react-three/drei';
+import {
+  Html,
+  OrbitControls,
+  PerspectiveCamera,
+  View,
+} from '@react-three/drei';
+
+import * as THREE from 'three';
 import Lights from './Lights';
 import IPhone from './IPhone';
 
@@ -8,7 +15,7 @@ const ModelView = ({
   groupRef,
   gsapType,
   controlRef,
-  setRotationSize,
+  setRotationState,
   size,
   item,
 }) => {
@@ -22,9 +29,28 @@ const ModelView = ({
       <ambientLight intensity={0.3} />
       <PerspectiveCamera makeDefault position={[0, 0, 4]} />
       <Lights />
-      <Suspense fallback={<div>Loading</div>}>
-        <IPhone />
-      </Suspense>
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      />
+      <group
+        ref={groupRef}
+        name={`${index === 1} ? "small" : "large`}
+        position={[0, 0, 0]}
+      >
+        <Suspense fallback={<div>Loading</div>}>
+          <IPhone
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
     </View>
   );
 };
