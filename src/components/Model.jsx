d4a542a61f4d9c852dcb31fcaa03b6ec148f9990
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-07-16 10:33:36 +0200

    feat: Enhance Model component with state management and 3D model controls
    - Added state management for model size and details.
      - Initialized size state with 'small'.
      - Set model state with title, color array, and image.
    - Introduced useRef hooks for camera control and 3D model groups.
      - Defined camerControlSmall and camerControlLarge refs for camera controls.
      - Created small and large refs for grouping 3D model components.
    - Added rotation state for small and large models.
      - Initialized smallRotation and largeRotation state variables.
    - Integrated ModelView components for rendering 3D models.
      - Configured first ModelView with small group, camera control, and rotation state.
      - Configured second ModelView with large group, camera control, and rotation state.
      - Passed index, groupRef, gsapType, controlRef, setRotation, item, and size as props to ModelView components.

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,7 +1,30 @@
+import { useRef, useState } from 'react';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
+import * as THREE from 'three';
+import ModelView from './ModelView';
+import { yellowImg } from '../utils';
 
 const Model = () => {
+  const [size, setSize] = useState('small');
+  const [model, setModel] = useState({
+    title: 'iPhone 15 Pro in Natural Titanium',
+    color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
+    img: yellowImg,
+  });
+
+  // camera control for the modal view
+  const camerControlSmall = useRef();
+  const camerControlLarge = useRef();
+
+  // group for the small and large model
+  const small = useRef(new THREE.Group());
+  const large = useRef(new THREE.Group());
+
+  // rotation value
+  const [smallRotation, setSmallRotation] = useState(0);
+  const [largeRotation, setLargeRotation] = useState(0);
+
   useGSAP(() => {
     gsap.to('#heading', {
       y: 0,
@@ -15,7 +38,27 @@ const Model = () => {
           Take a closer look.
         </h1>
         <div className="flex flex-col items-center mt-5">
-          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative"></div>
+          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+            <ModelView
+              index={1}
+              groupRef={small}
+              gsapType="view1"
+              controlRef={camerControlSmall}
+              setLargeRotation={setSmallRotation}
+              item={model}
+              size={size}
+            />
+
+            <ModelView
+              index={2}
+              groupRef={large}
+              gsapType="view2"
+              controlRef={camerControlLarge}
+              setLargeRotation={setLargeRotation}
+              item={model}
+              size={size}
+            />
+          </div>
         </div>
       </div>
     </section>
