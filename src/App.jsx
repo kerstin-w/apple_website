95efd39d7c0155de76eec71d438d7ee928aa3cd1
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-07-17 20:28:34 +0200

    feat: Add Footer component
    - Implemented a responsive Footer component with links and contact information.
    - Included a message with shopping options and an Apple support contact number.
    - Added a horizontal divider for visual separation.
    - Dynamically rendered footer links from a constants file.
    - Ensured text and links are styled with appropriate classes for consistency.

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import Highlights from './components/Highlights';
 import NavBar from './components/NavBar';
 import Model from './components/Model';
 import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
+import Footer from './components/Footer';
 
 import * as Sentry from '@sentry/react';
-import HowItWorks from './components/HowItWorks';
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
       <Model />
       <Features />
       <HowItWorks />
+      <Footer />
     </main>
   );
 };
